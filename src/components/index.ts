43a0cabb25192cbbcc5de7e8ed6e468c23a3daa0
Brandon Wijaya <brandon@softwareseni.com>
2023-10-20 23:53:19 +0700

    feat: add recommendation section component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
 import { Hero } from "./Hero";
 import { RecommendationCard } from "./RecommendationCard";
+import { Recommendation } from "./Recommendation";
 
-export { Navbar, Footer, Hero, RecommendationCard };
+export { Navbar, Footer, Hero, RecommendationCard, Recommendation };
