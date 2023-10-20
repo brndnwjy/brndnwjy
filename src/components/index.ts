fdc5cfa130f3e05483f250a2102b8b2d8c323263
Brandon Wijaya <brandon@softwareseni.com>
2023-10-21 01:18:50 +0700

    feat: add favorite card component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,13 @@ import { Navbar } from "./Navbar";
 import { Hero } from "./Hero";
 import { RecommendationCard } from "./RecommendationCard";
 import { Recommendation } from "./Recommendation";
+import { FavoriteCard } from "./FavoriteCard";
 
-export { Navbar, Footer, Hero, RecommendationCard, Recommendation };
+export {
+  Navbar,
+  Footer,
+  Hero,
+  RecommendationCard,
+  Recommendation,
+  FavoriteCard,
+};
